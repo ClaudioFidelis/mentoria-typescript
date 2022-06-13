03818e3f3fa4b58eb219d7d32554dbd89fe4dbef
Claudio <claudiofazevedo@gmail.com>
2022-06-13 16:23:36 -0300

    Resolvendo desafio 1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,38 @@
-console.log('Arquivo de testes. Pode mexer nele como quiser.')
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+interface employee {
+    code: number, 
+    name: string
+}
+
+let funcionarioA: employee = {
+    code: 1, 
+    name: "Antonio"
+}
+
+let funcionarioB: employee = {
+    code: 2,
+    name: "Victor"
+}
+
+
+class PessoaFisica {
+    codigo: number
+    nome: string
+
+    constructor(codigo: number, nome: string) {
+        this.codigo = codigo,
+        this.nome = nome
+    }
+}
+
+
+let funcionarioC: PessoaFisica = {
+    codigo: 3,
+    nome: "Marcos"
+}
+
+
+console.log(funcionarioA)
+console.log(funcionarioB)
+console.log(funcionarioC)
